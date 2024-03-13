01bab294039090f4893fbd860e1e16b86036b6b3
Divino Schaeffer <divino@Ordinateur-portable-de-Divino.local>
2024-03-13 15:36:46 +0100

    generation token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const User = require('../models/User')
+const User = require('../models/User');
+const JWT = require('jsonwebtoken');
+
+
+function generateToken(){
+    const secretKey = process.env.SECRET_KEY;
+    const options = {
+        expiresIn: '24h'
+    };
+    return JWT.sign({}, secretKey, options);
+}
 
 async function signIn(req, res){
 
@@ -14,10 +24,11 @@ async function signIn(req, res){
 
     try {
         const saved = await user.save();
-        res.status(200).json({user: {email: saved.email, groups: user.groups}});
+        const token = generateToken();
+        res.status(200).json({user: {_id: user._id, email: saved.email, groups: user.groups}, token: token});
     } catch (error) {
         console.log(error);
-        res.status(400).json("Erreur lors de l'enregistrement de l'utilisateur");
+        res.status(400).json({message: "Erreur lors de l'enregistrement de l'utilisateur"});
     }
     
 }
@@ -28,14 +39,15 @@ async function login(req, res){
     try {
         const user = await User.findOne({email});
         if(bcrypt.compare(password, user.password)){
-            res.status(200).json({user: {email: user.email, groups: user.groups}});
+            const token = generateToken();
+            res.status(200).json({user: {_id: user.id, email: user.email, groups: user.groups} , token: token});
         }
         else
             res.status(400).json("Mauvais mot de passe");
 
     } catch (error) {
         console.log(error);
-        res.status(400).json("Erreur lors de l'authentification");
+        res.status(400).json({message: "Erreur lors de l'authentification"});
     }
 }
 
