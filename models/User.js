7d7df17bc9dc72d4706f972986d32d669e614542
Divino Schaeffer <divino@Ordinateur-portable-de-Divino.local>
2024-03-13 17:00:33 +0100

    methods add and remove grade

--- models/User.js
+++ models/User.js
@@ -8,6 +8,24 @@ const userSchema = new Schema({
     groups: { type: [Schema.Types.ObjectId], default: [] }
 });
 
+userSchema.methods.addGroup = function(groupId) {
+    if (!this.groups.includes(groupId)) {
+        this.groups.push(groupId);
+        return this.save();
+    } else {
+        return Promise.reject(new Error('Le groupe est déjà ajouté à l\'utilisateur.'));
+    }
+};
+
+userSchema.methods.removeGroup = function(groupId) {
+    if (this.groups.includes(groupId)) {
+        this.groups.pull(groupId);
+        return this.save();
+    } else {
+        return Promise.reject(new Error('Le groupe n\'est pas associé à l\'utilisateur.'));
+    }
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
