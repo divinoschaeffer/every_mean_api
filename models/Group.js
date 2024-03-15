54a4e521cdc83a7f7fbdcc5bc0bf53fffa11e040
Divino Schaeffer <divino@Ordinateur-portable-de-Divino.local>
2024-03-15 14:22:29 +0100

    add methods addGrade, removeGrade, addGroup and static removeGroup

--- models/Group.js
+++ models/Group.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { startSession } = require('mongoose');
 
 const { Schema } = mongoose;
 
@@ -17,6 +18,58 @@ const groupSchema = new Schema({
     level: { type: Number, required: true },
     owner: { type: String, required: true }
 });
+
+groupSchema.methods.addGrade = function(gradeData) {
+    this.marks.push(gradeData);
+    return this.save();
+};
+
+groupSchema.methods.removeGrade = function(gradeId) {
+    const index = this.marks.findIndex(grade => grade._id === gradeId);
+    if (index !== -1) {
+        this.marks.splice(index, 1);
+        return this.save();
+    } else {
+        return Promise.reject(new Error('La note spécifiée n\'existe pas dans ce groupe.'));
+    }
+};
+
+groupSchema.methods.addGroup = function(groupId, session) {
+    if (!this.groups.includes(groupId)) {
+        this.groups.push(groupId);
+        return this.save({session});
+    } else {
+        return Promise.reject(new Error('Le groupe est déjà ajouté au groupe.'));
+    }
+};
+
+const removeGroupAndChildren = async (groupId, session) => {
+    const group = await Group.findById(groupId).session(session);
+    if (!group) {
+        return;
+    }
+
+    for (const childId of group.groups) {
+        await removeGroupAndChildren(childId, session);
+    }
+
+    await Group.findByIdAndDelete(group._id).session(session);
+};
+
+groupSchema.statics.removeGroup = async function(groupId, session) {
+    try {
+        await removeGroupAndChildren(groupId, session);
+
+        console.log('Groupe et ses enfants supprimés avec succès.');
+    } catch (error) {
+        if (session) {
+            await session.abortTransaction();
+            session.endSession();
+        }
+        console.error('Erreur lors de la suppression du groupe et de ses enfants:', error);
+    }
+};
+
 
 const Group = mongoose.model('Group', groupSchema);
 
