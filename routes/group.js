d396f8f62b2fabaf979a69eccada66d392c847c6
Divino Schaeffer <divino@Ordinateur-portable-de-Divino.local>
2024-03-16 13:12:31 +0100

    routes edit group, add and remove mark

--- routes/group.js
+++ routes/group.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createGroup, getGroup, deleteGroup } = require("../controllers/group");
+const { createGroup, getGroup, deleteGroup, editGroup, addMark, deleteMark } = require("../controllers/group");
 
 
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post('/create', createGroup);
 router.get('/:id', getGroup);
 router.delete('/delete/:id',deleteGroup);
+router.put('/edit/:id', editGroup);
+router.put('/add-mark/:id', addMark);
+router.delete('/mark/delete/:id', deleteMark);
 
 module.exports = router;
